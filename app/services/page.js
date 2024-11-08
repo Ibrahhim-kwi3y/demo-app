e94f3af7499f6e8797c09b54a5cecfaf9f419602
Ibrahim Elkwaiey <55067366+Ibrahhim-kwi3y@users.noreply.github.com>
2024-11-09 01:40:35 +0200

    update

--- app/services/page.js
+++ app/services/page.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-// import Todo from "../component/todos";
+import Todo from "../component/todos";
 
 
 
@@ -9,7 +9,7 @@ export default function Services()
         <>
         <Link href="/posts">Browse Blogs</Link>
 
-        {/* <Todo /> */}
+        <Todo />
         </>
     )
 }
