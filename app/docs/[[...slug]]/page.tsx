c0cf7ce26e04a20398aa26d173bba5a88c5d4934
Ibrahim Elkwaiey <55067366+Ibrahhim-kwi3y@users.noreply.github.com>
2024-11-09 01:09:00 +0200

    Update page.tsx

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -1,22 +1,24 @@
+// Assuming params always contains an optional slug array
 import React from "react";
 
-const Docs = async ({ params }: { params: { slug?: string[] } }) => {
-    if (!params) {
-        // Await params to make sure it's fully loaded before accessing properties
-        await new Promise(resolve => setTimeout(resolve, 0));
-    }
+interface PageProps {
+  params: {
+    slug?: string[];
+  };
+}
 
-    if (params.slug?.length === 2) {
-        return (
-            <h1>
-                Blog For Category {params.slug[0]} for title {params.slug[1]}
-            </h1>
-        );
-    } else if (params.slug?.length === 1) {
-        return <h1>Blog For Category {params.slug[0]}</h1>;
-    } else {
-        return <h1>Blog Home Page</h1>;
-    }
+const Docs = ({ params }: PageProps) => {
+  if (params.slug?.length === 2) {
+    return (
+      <h1>
+        Blog For Category {params.slug[0]} for title {params.slug[1]}
+      </h1>
+    );
+  } else if (params.slug?.length === 1) {
+    return <h1>Blog For Category {params.slug[0]}</h1>;
+  } else {
+    return <h1>Blog Home Page</h1>;
+  }
 };
 
 export default Docs;
